b723a5d8e465572608fb82e3be69be818210627c
ComtK <kjs20305@gmail.com>
2022-01-04 16:55:22 +0900

    add route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
 
 const App = () => {
 	return (
-		<div>
-			<h3>Hello World</h3>
-		</div>
+		<Routes>
+			<Route path={'/'} element={<Home />} />
+			<Route path={'/about'} element={<About />} />
+		</Routes>
 	);
 };
 
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+	return (
+		<div>
+			home
+			<br />
+			<Link to={'/about'}>about</Link>
+		</div>
+	);
+};
+
+export default Home;
